3bc2fe7e3bc0881229b80dddceafc8553d4bbcd5
Alisalee Aaron <aaaron@brave.com>
2021-09-01 01:03:44 -0400

    Removes duplicate files from data file and creates excel sheet of names.

--- env/compare.py
+++ env/compare.py
@@ -4,7 +4,7 @@ Read File of Folders
 Compare to see if there are any names that are not in the folder and vice versa
 '''
 import pandas as pd
-from os import listdir
+from os import listdir, remove
 from os.path import isfile, join
 import re
 
@@ -18,21 +18,31 @@ def getFolderNames():
     rawFileNames = listdir("Phase 1")
     return rawFileNames
 
-def createComparisonSheet(excelNames, fileNames):
+def deleteRepeats(fileNames):
     count = []
+    original = []
     for name in fileNames:
         res = name[len(name) - 7 : len(name) - 4]
         if re.match("\(\d*\)", res) != None:
+            remove("Phase 1/" + name)
             count.append(name)
             print(name)
+        else:
+            original.append(name)
     print("duplicate amount: " + str(len(count)))
+    return original
+
+def createDataSheet(originalNames):
+    frame = pd.DataFrame(originalNames, columns=["File Names"])
+    frame.to_excel("Phase_1_Names.xlsx")
 
 def main():
     excelNames = getExcelNames()
     excelNames.sort()
     fileNames = getFolderNames()
     fileNames.sort()
-    createComparisonSheet(excelNames, fileNames)
+    original = deleteRepeats(fileNames)
+    createDataSheet(original)
     print("done")
 
 
